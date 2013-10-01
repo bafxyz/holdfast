d6785a1ca71e591a1749e8c73bb6a14f7a131cda
Andrei Bunulu <bunulu@gmail.com>
2013-10-01 20:24:38 +0300

    develop - First state

---
+++ .compass.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins installed on your system.
+require 'compass-normalize'
+require 'rgbapng'
+require 'toolkit'
+require 'susy'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "app/styles"
+sass_dir = "app/styles"
+images_dir = "app/images"
+javascripts_dir = "app/scripts/"
---
+++ app/scripts/main.js
@@ -0,0 +1,114 @@
+$(function() {
+
+  var sliderWrapper = $('.slider-wrapper');
+  var slider =  sliderWrapper.children('.rslides');
+  var slogan = $('.slogan');
+  var menu = $('.m-menu');
+
+  // Caption positioning
+  function sliderCaption() {
+    if( $(document).width() < 768 ) {
+      if ( sliderWrapper.children('.caption').length === 0 ) {
+        slider.find('.caption').clone().appendTo(sliderWrapper);
+      }
+    } else {
+      if ( sliderWrapper.children('.caption').length > 0 ) {
+        sliderWrapper.children('.caption').remove();
+      }
+    }
+  }
+
+  // Slider positioning
+  function sloganPosition() {
+    slogan.css('margin-top', sliderWrapper.height());
+  }
+
+  // Mobile menu
+  function mobileMenu () {
+    if ($(document).width() > 768) {
+      menu.hide();
+    } else {
+      menu.css('height', $(window).height());
+
+      if (menu.find('ul').length === 0 ) {
+        $('.menu > ul').clone().appendTo(menu);
+      }
+    }
+  }
+
+  // News SLider
+  $('.bxslider').bxSlider({
+    auto: true,
+    autoControls: false,
+    pager: false,
+    minSlides: 1,
+    maxSlides: 1,
+    moveSlides: 1,
+    slideMargin: 1
+  });
+
+  // Slider
+  $('.rslides').responsiveSlides({
+    auto: true,
+    pager: true,
+    nav: false,
+    speed: 500,
+    before: function() {
+    },
+    after: function() {
+      var currentSlide = $('.rslides_here').index();
+      sliderWrapper.children('.caption').removeClass('here');
+      sliderWrapper.children('.caption').eq(currentSlide).addClass('here');
+      sloganPosition();
+    }
+  });
+
+  // Init
+  sloganPosition();
+  mobileMenu();
+
+  // SLider Labels
+  sliderWrapper.find('.rslides_tabs li').each(function (index) {
+    $(this).find('a').html(slider.find('li').eq(index).find('.name').html());
+  });
+
+  $(window).resize(function() {
+    sliderCaption();
+    sloganPosition();
+    mobileMenu();
+  });
+
+  // Placeholder for IE
+  $('[placeholder]').focus(function() {
+    var input = $(this);
+    if (input.val() === input.attr('placeholder')) {
+      input.val('');
+      input.removeClass('placeholder');
+    }
+  }).blur(function() {
+    var input = $(this);
+    if (input.val() === '' || input.val() === input.attr('placeholder')) {
+      input.addClass('placeholder');
+      input.val(input.attr('placeholder'));
+    }
+  }).blur().parents('form').submit(function() {
+    $(this).find('[placeholder]').each(function() {
+      var input = $(this);
+      if (input.val() === input.attr('placeholder')) {
+        input.val('');
+      }
+    });
+  });
+
+  // Back to top
+  $('.top-link').click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+  });
+
+  // Toggle menu
+  $('.m-menu-btn').click(function (e) {
+    e.preventDefault();
+    menu.toggle();
+  });
+});
