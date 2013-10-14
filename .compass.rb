177937b1b740f5d2121c34fd81cfb5c23dc269aa
Andrei Bunulu <bunulu@gmail.com>
2013-10-14 22:41:58 +0300

    pages - AB some checkout changes

--- .compass.rb
+++ .compass.rb
@@ -1,12 +1,72 @@
-# Require any additional compass plugins installed on your system.
+# ==============================================================================
+# REQUIRED 3RD PARTY COMPASS EXTENSIONS
+# ==============================================================================
 require 'compass-normalize'
 require 'rgbapng'
 require 'toolkit'
 require 'susy'
 
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "app/styles"
-sass_dir = "app/styles"
-images_dir = "app/images"
-javascripts_dir = "app/scripts/"
+
+# ==============================================================================
+# COMPASS PROJECT CONFIGURATION
+# ==============================================================================
+
+# Can be `:stand_alone` or `:rails`. Defaults to `:stand_alone`.
+project_type = :stand_alone
+
+# To enable relative paths to assets via compass helper functions.  Please note
+# that this will *only* allow you to use relative URLs for the image_url SASS
+# function, and not compute relative URLs for you.
+#
+# Uncomment:
+relative_assets       = true
+
+# Indicates whether line comments should be added to compiled css that says
+# where the selectors were defined.
+line_comments         = false
+
+# The output style for the compiled css.  One of: `:nested`, `:expanded`,
+# `:compact`, or`:compressed`.
+output_style          = :compact
+
+# ==============================================================================
+# COMPASS SOURCE DIRECTORY CONFIGURATION
+# ==============================================================================
+
+# Directory containing the SASS source files
+sass_dir              = "app/styles"
+
+# Directory where Compass dumps the generated CSS files
+css_dir               = "app/styles"
+
+# Directory where font files use in font-face declarations are stored
+fonts_dir             = "app/styles/fonts"
+
+# Directory where Compass stores the Grid image, and the sites images are stored
+images_dir            = "app/images"
+
+# Directory where the sites' JavaScript file are stored
+javascripts_dir       = "app/scripts"
+
+# ==============================================================================
+# COMPASS TARGET DIRECTORY CONFIGURATION
+# ==============================================================================
+
+# The root of all operations, must be set for Compass to work.
+http_path             = "/"
+
+# Directory where Compass dumps the generated CSS files
+http_css_path         = http_path + "assets/"
+
+# Directory where font files use in font-face declarations are stored
+http_fonts_path       = http_path + "assets/"
+
+# Directory where Compass stores the Grid image, and the sites images are stored
+http_images_path      = http_path + "assets/"
+
+# Directory where the sites' JavaScript file are stored
+http_javascripts_path = http_path + "assets/"
+
+# ==============================================================================
+# THE END
+# ==============================================================================
